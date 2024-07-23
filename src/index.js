31d538fe7b5fc8c998028bd1d6acf090f62044ac
kcodes57 <kir652986@gmail.com>
2024-07-23 17:37:12 -0500

    created real data for weather description

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 function refreshWeatherData(response) {
   let temperatureElement = document.querySelector("#temp");
   let temp = Math.round(response.data.temperature.current);
+  let descriptionElement = document.querySelector("#description");
+  description = response.data.condition.description;
+  descriptionElement.innerHTML = description;
+
   temperatureElement.innerHTML = temp;
   let cityElement = document.querySelector("#city");
   let city = response.data.city;
   cityElement.innerHTML = city;
 }
+
 function searchCity(city) {
   let apiKey = "441fcacb9dt020b37114da0ba3e3f5of";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
