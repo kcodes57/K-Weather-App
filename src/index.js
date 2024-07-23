486bd841aad5aa03f254eda279f0beb5db4f96d3
kcodes57 <kir652986@gmail.com>
2024-07-23 17:22:19 -0500

    added api for live data
    added live data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+function refreshWeatherData(response) {
+  let temperatureElement = document.querySelector("#temp");
+  let temp = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temp;
+  let cityElement = document.querySelector("#city");
+  let city = response.data.city;
+  cityElement.innerHTML = city;
+}
+function searchCity(city) {
+  let apiKey = "441fcacb9dt020b37114da0ba3e3f5of";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeatherData);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
-
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
