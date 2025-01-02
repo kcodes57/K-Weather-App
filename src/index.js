e3bbed13cbee2c49ff2679418e43afcf6fc02ec7
kcodes57 <kir652986@gmail.com>
2025-01-02 13:43:33 -0600

    added ID for unit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function refreshWeatherData(response) {
 
   let temperatureElement = document.querySelector("#temp");
   let timeElement = document.querySelector("#time");
+  let unitElement = document.querySelector("#unit");
   let windElement = document.querySelector("#wind");
 
   let city = response.data.city;
@@ -13,8 +14,16 @@ function refreshWeatherData(response) {
   let description = response.data.condition.description;
   let iconImage = `<img src="${response.data.condition.icon_url}" alt=""/>`;
   let temp = Math.round(response.data.temperature.current);
+  let unit = prompt("Please type C for Celsius or F for fahrenheit");
+  let unitLowerCase = unit.toLowerCase();
   let humidity = response.data.temperature.humidity;
   let wind = response.data.wind.speed;
+
+  if (unit === "c") {
+    getForecastCelsius(response.data.city);
+  } else {
+    getForecastFahrenheit(response.data.city);
+  }
 
   cityElement.innerHTML = city;
   descriptionElement.innerHTML = description;
@@ -23,7 +32,11 @@ function refreshWeatherData(response) {
   temperatureElement.innerHTML = temp;
   timeElement.innerHTML = formateDate(date);
   windElement.innerHTML = ` ${wind} km/h`;
-  getForecast(response.data.city);
+  if (unit < 2) {
+    getForecastCelsius(response.data.city);
+  } else {
+    getForecastFahrenheit(response.data.city);
+  }
 }
 
 function formateDate(date) {
@@ -45,22 +58,36 @@ function formateDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function searchCity(city) {
+function searchCityCelsius(city) {
   let apiKey = "441fcacb9dt020b37114da0ba3e3f5of";
 
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeatherData);
+}
+
+function searchCityFahrenheit(city) {
+  let apiKey = "441fcacb9dt020b37114da0ba3e3f5of";
+
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(refreshWeatherData);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
+
   searchCity(searchInput.value);
 }
 
-function getForecast(city) {
+function getForecastCelsius(city) {
   let apiKey = "441fcacb9dt020b37114da0ba3e3f5of";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function getForecastFahrenheit(city) {
+  let apiKey = "441fcacb9dt020b37114da0ba3e3f5of";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -104,6 +131,15 @@ function displayForecast(response) {
 }
 
 let searchFormElement = document.querySelector("#search-form");
+let unit = prompt(
+  "Do you want data shown in Celsius or Fahrenheit? Please enter 1 for Celsius or 2 for Fahrenheit."
+);
+
+function weatherUnit(prompt) {}
 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-searchCity("Paris");
+if (unit < 2) {
+  searchCityCelsius("Paris");
+} else {
+  searchCityFahrenheit("Minneapolis");
+}
